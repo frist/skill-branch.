286e97983a1b53ce8dcc716c5ce7c1f67f991c1d
Elena Rusakova <elena.v.rus@gmail.com>
2016-11-11 21:29:12 +0300

    task2B FIO

--- src/index.js
+++ src/index.js
@@ -14,6 +14,30 @@ app.get('/task2A', (req, res) => {
   res.send(sum.toString())
 });
 
+app.get('/task2B', (req, res) => {
+  console.log(req.query.fullname)
+  try {
+    const fullname = req.query.fullname
+    if(!fullname)
+      throw 'Invalid fullname'
+
+    const reg = /\S+/g
+
+    const names = fullname.match(reg);
+    console.log(names)
+    if(names.length > 3 || names.length == 0)
+      throw 'Invalid fullname'
+
+    let fio = names[names.length - 1]
+    for(let i = 0; i < names.length -1; i++)
+      fio += " " + names[i].substr(0,1) + "."
+
+    res.send(fio)
+  } catch(e) {
+    res.send(e)
+  }
+});
+
 app.listen(3000, () => {
   console.log('Your app listening on port 3000!');
 });
